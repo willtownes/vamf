d9391d773fd3d75322d49eee1885d99c90236e4f
Will Townes <will.townes@gmail.com>
2017-08-16 11:59:37 -0400

    move example code to roxygen hoping will display in the help file

--- R/vamf.R
+++ R/vamf.R
@@ -204,7 +204,19 @@ ortho_extract<-function(stfit,ss){
 #' @param svmult Scalar or vector of multipliers to increase or decrease the sigma_v scale hyperparameter.
 #' @return List of posterior means of all model parameters. The 'factors' and 'loadings' are analogous to PCA. Cell positions in latent space can be plotted by using the 'factors' matrix.
 #' @export
-#' @example inst/examples/vamf_examples.R
+#' @example N<-20; G<-60; Gsignal<-20; Gnoise<-G-Gsignal
+#' theta<-seq(from=0,to=2*pi,length.out=N)
+#' true_cell_positions<-data.frame(dim1=3*cos(theta),dim2=3*sin(theta))
+#' with(true_cell_positions,plot(dim1,dim2))
+#' informative_rows<-as.matrix(true_cell_positions)%*%matrix(rnorm(Gsignal*2),nrow=2)
+#' noise_rows<-matrix(rnorm(Gnoise*N),nrow=Gnoise)
+#' Y<-rbind(t(informative_rows),noise_rows)+rnorm(G)+10
+#' Z<-matrix(rbinom(G*N,1,.8),nrow=G)
+#' Y<-Y*Z
+#' pca_factors<-prcomp(t(Y),center=TRUE,scale=TRUE)$x
+#' plot(pca_factors[,1:2])
+#' vamf_factors<-vamf(Y,5,nrestarts=2,log2trans=FALSE)$factors
+#' with(vamf_factors,plot(dim1,dim2))
 vamf<-function(Y, L, nrestarts=4, log2trans=TRUE, pseudocount=0.0, output_samples=100, save_restarts=FALSE,svmult=1){
   #convenience wrapper for running instances of selection factorization
   ss<-init_ss(Y,L,log2trans=log2trans,pseudocount=pseudocount)
